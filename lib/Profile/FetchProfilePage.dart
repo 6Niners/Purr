a706401efbcbb3b86e8df003f107c4ae72866162
Karim Bastami <Karimawesome7@gmail.com>
2020-12-18 19:29:49 +0200

    Profile Data Updates from reload button
    (this is me testing stateful widgets)
    
    - added reload button
    
    - Profile data changes every time you press the reload button

--- lib/Profile/FetchProfilePage.dart
+++ lib/Profile/FetchProfilePage.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 
 
+
+List<String> filteredDummyInfoList = ["Karim", "CoolDude", "Karim@6Niners.com"];
+
+List<String> dummyInfoList = ["Friskie", "Sherazy", "Friskie@6Niners.com",
+                              "Fanta", "Sherazy", "Fanta@6Niners.com",
+                              "Arya", "Gay", "AmadeusGay@6Niners.com",
+                              "Zezo", "SmallFishark", "Zuz@6Niners.com"];
+
+
+
+List<String> returnProfileData(int counter) {
+
+  for (int i = 0; i < 3; i++) {
+    filteredDummyInfoList[i] = dummyInfoList[i + counter];
+  }
+
+  return filteredDummyInfoList;
+}
+
+
+
 class FetchProfilePage extends StatefulWidget {
   @override
   _FetchProfilePageState createState() => _FetchProfilePageState();
 }
 
-
 class _FetchProfilePageState extends State<FetchProfilePage> {
+
+  int counter = -3;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +41,26 @@ class _FetchProfilePageState extends State<FetchProfilePage> {
         backgroundColor: Colors.blueGrey[800],
         title: Text("Profile"),
         elevation: 0.0,
+      ),
+
+
+      floatingActionButton: FloatingActionButton.extended(
+          label: Text("Update"),
+          icon: Icon(Icons.wifi_protected_setup),
+          backgroundColor: Colors.blueGrey[800],
+          onPressed: (){
+            setState(() {
+
+              if (counter < 9){
+                counter += 3;
+              }
+              else{
+                counter = 0;
+              }
+
+              returnProfileData(counter);
+            });
+          },
       ),
 
 
@@ -45,7 +88,7 @@ class _FetchProfilePageState extends State<FetchProfilePage> {
 
             SizedBox(height: 5.0),
 
-            Text("Zezo", style: TextStyle(
+            Text(filteredDummyInfoList[0], style: TextStyle(
               color: Colors.amberAccent[200],
               fontSize: 23.0,
               fontWeight: FontWeight.bold,
@@ -59,7 +102,7 @@ class _FetchProfilePageState extends State<FetchProfilePage> {
 
             SizedBox(height: 5.0),
 
-            Text("Smol Fishark", style: TextStyle(
+            Text(filteredDummyInfoList[1], style: TextStyle(
               color: Colors.amberAccent[200],
               fontSize: 23.0,
               fontWeight: FontWeight.bold,
@@ -73,7 +116,7 @@ class _FetchProfilePageState extends State<FetchProfilePage> {
 
                 SizedBox(width: 10),
 
-                Text("YoMommaCool@6Niners.co", style: TextStyle(
+                Text(filteredDummyInfoList[2], style: TextStyle(
                   fontSize: 18.0,
                   color: Colors.white,
 
