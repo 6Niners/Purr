8ac9af67c0f42d3af9258b75d683fc0542ab4347
zezo <zezohassam@gmail.com>
2020-12-26 15:50:12 +0200

    Firebase.initializeApp() Changed its order

--- lib/MainController.dart
+++ lib/MainController.dart
@@ -2,13 +2,14 @@ import 'package:get/get.dart';
 import 'package:purr/MainPage/MainPageController.dart';
 import 'package:purr/Registration/RegistrationController.dart';
 import 'package:purr/Services/Database.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 
 class MainController extends GetxController {
 
 
   @override
   Future<void> onInit() async {
+    Firebase.initializeApp();
     Get.put(MainPageController());
     Get.put(RegistrationController());
     Get.put(DatabaseService());
--- lib/Registration/RegistrationController.dart
+++ lib/Registration/RegistrationController.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:purr/MainPage/MainPage.dart';
@@ -15,7 +15,6 @@ class RegistrationController extends GetxController {
 
   @override
   Future<void> onInit() async {
-    Firebase.initializeApp();
     super.onInit();
   }
 
